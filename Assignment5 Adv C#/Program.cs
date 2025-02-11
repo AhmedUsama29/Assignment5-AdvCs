c3bbae9d5ff372792bd91eb34e0d6055c29f1815
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-11 18:39:25 +0200

    Regions

--- Assignment5 Adv C#/Program.cs
+++ Assignment5 Adv C#/Program.cs
@@ -4,42 +4,53 @@
     {
         //delegate declaration
         public delegate string BookDetails(Book B);
+
         static void Main(string[] args)
         {
+            #region List
             List<Book> books = new List<Book>
             {
                 new Book("ISBN1", "OOP ", new[] { "Ahmed Osama", "Kareem Osama" }, new DateTime(2020, 1, 1), 30),
                 new Book("ISBN2", "Advanced C#", new[] { "Mohammed Elsayed" }, new DateTime(2022, 5, 15), 60)
             };
+            #endregion
 
+            #region My User-Defind Delegate
             Console.WriteLine("==================================");
             Console.WriteLine("My Delegate");
-            //My Delegate
+
             BookDetails TitleDelegate = BookFunction.GetTitle;
             LibraryEngine.ProcessBooks(books, TitleDelegate);
+            #endregion
 
+            #region Built-in (Func) Delegate
             Console.WriteLine("==================================");
             Console.WriteLine("Func Delegate");
 
-            //Built in Delegate (Func)
             Func<Book, string> AuthorsDelegate = BookFunction.GetAuthors;
-            LibraryEngine.ProcessBooks(books, new BookDetails(AuthorsDelegate)); //Convert Func to delegate
+            LibraryEngine.ProcessBooks(books, new BookDetails(AuthorsDelegate)); //Convert Func to delegate 
+            #endregion
 
+            #region Anonymous Method
             Console.WriteLine("==================================");
             Console.WriteLine("Anonymous Delegate");
-            //Anonymous Method
-            BookDetails ISBNDelegate = delegate (Book B) 
+
+            BookDetails ISBNDelegate = delegate (Book B)
             {
-                return $"ISBN: {B.ISBN}"; 
+                return $"ISBN: {B.ISBN}";
             };
 
             LibraryEngine.ProcessBooks(books, ISBNDelegate);
 
+            #endregion
+
+            #region Lambda Expression (Goes To)
             Console.WriteLine("==================================");
             Console.WriteLine("Lambda Expression");
-            //Lambda Expression (Goes To)
+
             BookDetails PubDateDelegate = B => $"Publication Date: {B.PublicationDate.ToShortDateString()}";
-            LibraryEngine.ProcessBooks(books, PubDateDelegate);
+            LibraryEngine.ProcessBooks(books, PubDateDelegate); 
+            #endregion
 
         }
     }
